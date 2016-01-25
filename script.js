6f6bea628f421f1eab6a26cdb8146e6c27389860
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2016-01-25 11:23:16 +0200

    entering html and array

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var currency = {
+	USD: {
+		img: '',
+		attirudeToUSD: '1',
+		name: 'USD'
+	},
+	EUR: {
+		img: '',
+		attirudeToUSD: '1.08',
+		name: 'EUR'
+	},
+	GBR: {
+		img: '',
+		attirudeToUSD: '1.42',
+		name: 'GBR'
+	},
+	CAD: {
+		img: '',
+		attirudeToUSD: '0.70'
+		name: 'CAD'
+	},
+	CNY: {
+		img: '',
+		attirudeToUSD: '0.15'
+		name: 'CNY'
+	},
+	RUB: {
+		img: '',
+		attirudeToUSD: '78'
+		name: 'RUB'
+	},
+	UAH: {
+		img: '',
+		attirudeToUSD: '26'
+		name: 'UAH'
+	},
+	MXN: {
+		img: '',
+		attirudeToUSD: '0,05'
+		name: 'MXN'
+	},
+	
+
+}
