ed849f39e9a9206758ac36a9e4d7f21ca04b8e50
Vadim Lavorchuk <lavorchukvadim@yandex.ua>
2016-02-01 00:46:52 +0200

    ну вродь все только обрабоку инпута и акции с чеком осталось сделать

--- script.js
+++ script.js
@@ -8,73 +8,111 @@ var currency = {
         },
         EUR: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/810px-Flag_of_Europe.svg.png',
-            attirudeToUSD: '1.08',
+            attirudeToUSD: '0.92310',
             name: 'EUR'
         },
-        GBR: {
+        GBP: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png',
-            attirudeToUSD: '1.42',
-            name: 'GBR'
+            attirudeToUSD: '0.70183',
+            name: 'GBP'
         },
         CAD: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/1000px-Flag_of_Canada.svg.png',
-            attirudeToUSD: '0.70',
+            attirudeToUSD: '1.39705',
             name: 'CAD'
         },
         CNY: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Flag_of_Eritrea.svg/1000px-Flag_of_Eritrea.svg.png',
-            attirudeToUSD: '0.15',
+            attirudeToUSD: '6.57428',
             name: 'CNY'
         },
         RUB: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/900px-Flag_of_Russia.svg.png',
-            attirudeToUSD: '78',
+            attirudeToUSD: '75.4620',
             name: 'RUB'
         },
         UAH: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/1024px-Flag_of_Ukraine.svg.png',
-            attirudeToUSD: '26',
+            attirudeToUSD: '25.5200',
             name: 'UAH'
         },
         MXN: {
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/840px-Flag_of_Mexico.svg.png',
-            attirudeToUSD: '0.05',
+            attirudeToUSD: '18.1045',
             name: 'MXN'
         }
     }
-    // переменные
+
+var curLength = (function() {
+	var i = 0;
+	for (var item in currency) {
+		i++;
+	}
+	return i;
+})();
+// переменные
 var selectFrom = document.getElementById('currencyFrom'),
     selectTo = document.getElementById('currencyTo'),
     imageNearFrom = document.getElementById('imgFrom'),
     imageNearTo = document.getElementById('imgTo'),
-    conBut = document.getElementsByTagName('button')[0],
-    result = document.getElementById('result');
-    
+    swap = document.getElementsByTagName('button')[0],
+    result = document.getElementById('result'),
+    number = document.getElementById('number'),
+    convertor = document.getElementById('convertor');
+
 // вызов функций
-makeSelect(selectFrom);
-makeSelect(selectTo);
-findAndChange(selectFrom, imageNearFrom);
-findAndChange(selectTo, imageNearTo);
+makeSelect(selectFrom); // делаю селект
+makeSelect(selectTo);	// делаю селект
+findAndChange(selectFrom, imageNearFrom); // это для начального отображения картинки
+findAndChange(selectTo, imageNearTo); // это для начального отображения картинки
 // обработчики
-selectFrom.addEventListener('change', function() {
+selectFrom.addEventListener('change', function() { // обработчик на смену селекта
     findAndChange(selectFrom, imageNearFrom);
 });
-selectTo.addEventListener('change', function() {
+selectTo.addEventListener('change', function() { // обработчик на смену селекта
     findAndChange(selectTo, imageNearTo);
 });
-conBut.addEventListener('click', function() {
-	var benefits = document.getElementById('benefits'),
-	    draft = document.getElementById('draft'),
-	    number = document.getElementById('number').value,
-	    from = selectFrom.options[selectFrom.selectedIndex].value,
-	    to = selectTo.options[selectTo.selectedIndex].value,
-	    res = 0;
-	res = number / from * to;
-	res = res.toFixed(1);
-	result.innerHTML = '<p>Result: ' + res + '</p>';
-});
+convertor.addEventListener('input', function() { // делегирую нажатия на инпут[текст] и селекты
+    var target = event.target,
+        benefits = 0,
+        draft = 0,
+        numberV = 0,
+        from = 0,
+        to = 0,
+        res = 0;
+    if (target == number || selectFrom || selectTo) {
+    	numberV = document.getElementById('number').value;
+        from = selectFrom.options[selectFrom.selectedIndex].value;
+        to = selectTo.options[selectTo.selectedIndex].value;
+        res = (numberV * to / from).toFixed(5);
+        result.innerHTML = '<p>Result: ' + res + '</p>';
+    }
+})
+swap.addEventListener('click', function() { // обработчик на кнопку "свап" 
+	var buffer = '';
+	var benefits = 0,
+        draft = 0,
+        numberV = 0,
+        from = 0,
+        to = 0,
+        res = 0;
+    // меняю местами индексы в селектах
+    buffer = selectFrom.selectedIndex
+    selectFrom.selectedIndex = selectTo.selectedIndex;
+    selectTo.selectedIndex = buffer;
+    // меняю местами картинки
+	buffer = imageNearFrom.src;
+	imageNearFrom.src = imageNearTo.src;
+	imageNearTo.src = buffer;
+	// обновляю результат
+	numberV = document.getElementById('number').value;
+   	from = selectFrom.options[selectFrom.selectedIndex].value;
+    to = selectTo.options[selectTo.selectedIndex].value;
+    res = (numberV * to / from).toFixed(5);
+    result.innerHTML = '<p>Result: ' + res + '</p>';
+})
 // функкии
-function makeSelect(elem) {
+function makeSelect(elem) { // заполняет селект
     for (var item in currency) {
         var option = document.createElement("option");
         option.text = currency[item].name;
@@ -83,7 +121,7 @@ function makeSelect(elem) {
     }
 }
 
-function findAndChange(se, im) {
+function findAndChange(se, im) { // картинки делает
     var choosen = se.options[se.selectedIndex].value;
     for (var item in currency) {
         if (choosen == currency[item].attirudeToUSD) {
